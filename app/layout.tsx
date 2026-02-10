938c93c07c26174479eb501e178922855612bfb4
MoFahimKh <fk0534872@gmail.com>
2026-02-10 21:25:12 +0530

    feat: add macan fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,28 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { Providers } from "@/app/providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"]
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"]
+const macan = localFont({
+  src: [
+    {
+      path: "../fonts/MacanPanWeb-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/MacanPanWeb-Medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/MacanPanWeb-Semibold.woff",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-macan",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +37,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${macan.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
     </html>
